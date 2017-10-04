ddc828d51cfad972ccc31a258e4406b0fa0b45c6
Anders Rydbirk <ary@it-minds.dk>
2017-10-04 11:50:06 +0200

    Next version ready

--- a3c-cpu/main.py
+++ a3c-cpu/main.py
@@ -2,18 +2,19 @@ import tensorflow as tf
 import threading
 import multiprocessing
 import os
-from time import sleep
+from time import sleep, time
 
 from Network import Network
 from Worker import Worker
 
 max_episode_length = 300
-gamma = .99 # discount rate for advantage estimation and reward discounting
+max_buffer_length = 20
+gamma = .99  # discount rate for advantage estimation and reward discounting
 height = 210
 width = 160
 depth = 3
-s_size = height * width * depth # Observations are greyscale frames of 84 * 84 * 1
-a_size = 4 # Agent can move Left, Right, or Fire
+s_size = height * width * depth  # Observations are greyscale frames of 84 * 84 * 1
+a_size = 4  # Agent can move Left, Right, or Fire
 load_model = False
 model_path = './model'
 game_env = 'Breakout-v0'
@@ -25,19 +26,22 @@ if not os.path.exists(model_path):
 
 with tf.device("/cpu:0"):
     global_episodes = tf.Variable(0, dtype=tf.int32, name='global_episodes', trainable=False)
-    trainer = tf.train.AdamOptimizer(learning_rate=1e-4)
+    trainer = tf.train.RMSPropOptimizer(learning_rate=7e-4, epsilon=0.1, decay=0.99)
     master_network = Network(height, width, depth, s_size, a_size, 'global', None)  # Generate global network
     num_workers = multiprocessing.cpu_count()  # Set workers ot number of available CPU threads
+
     print 'Creating', num_workers, 'workers'
     workers = []
     # Create worker classes
     for i in range(num_workers):
-        workers.append(Worker(game_env, i, (height, width, depth, s_size), a_size, trainer, model_path, global_episodes))
+        workers.append(
+            Worker(game_env, i, (height, width, depth, s_size), a_size, trainer, model_path, global_episodes))
     saver = tf.train.Saver(max_to_keep=5)
 
 with tf.Session() as sess:
     coord = tf.train.Coordinator()
-    if load_model == True:
+
+    if load_model:
         print ('Loading Model...')
         ckpt = tf.train.get_checkpoint_state(model_path)
         saver.restore(sess, ckpt.model_checkpoint_path)
@@ -48,9 +52,17 @@ with tf.Session() as sess:
     # Start the "work" process for each worker in a separate threat.
     worker_threads = []
     for worker in workers:
-        worker_work = lambda: worker.work(max_episode_length, gamma, sess, coord, saver)
+        worker_work = lambda: worker.work(max_episode_length, gamma, sess, coord, saver, max_buffer_length)
         t = threading.Thread(target=(worker_work))
         t.start()
         sleep(0.5)
         worker_threads.append(t)
+    gs = 0
+
+    while not coord.should_stop():
+        s = time()
+        sleep(10)
+        gs1 = sess.run(global_episodes)
+        print("Episodes", gs1, 'one for ', (time() - s) / (gs1 - gs))
+        gs = gs1
     coord.join(worker_threads)
